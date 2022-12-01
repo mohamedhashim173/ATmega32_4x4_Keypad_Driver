3ab7bdca4ae38f335eed706d0fc33d1958d61251
Mohamed Hashim <102317409+mohamedhashim173@users.noreply.github.com>
2022-12-01 22:10:46 +0200

    Add files via upload

---
+++ KEYPAD_private.h
@@ -0,0 +1,14 @@
+/*
+ * Keypad_private.h
+ *
+ *  Created on: Oct 7, 2022
+ *      Author: Mohamed Hashim Attiya Said
+ */
+
+#ifndef KEYPAD_PRIVATE_H_
+#define KEYPAD_PRIVATE_H_
+
+#define KEYPAD_ROWS_NUM		4
+#define KEYPAD_COLS_NUM		4
+
+#endif /* KEYPAD_PRIVATE_H_ */
---
+++ main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 7, 2022
+ *      Author: MSAEED99
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+// MCAL
+#include "DIO_interface.h"
+
+// HAL
+#include "LCD_interface.h"
+#include "KEYPAD_interface.h"
+
+void main(void)
+{
+	u8 local_u8KeyVal;
+
+	LCD_voidInit();
+
+	KEYPAD_voidPinInit();
+
+	while(1)
+	{
+
+
+		local_u8KeyVal = KEYPAD_u8GetPressedKey();
+
+		if(local_u8KeyVal != 0xFF && local_u8KeyVal != 'c')
+		{
+			LCD_voidSendCharacter(local_u8KeyVal);
+		}
+		else if(local_u8KeyVal == 'c')
+		{
+			LCD_voidInit();
+		}
+	}
+}
+
